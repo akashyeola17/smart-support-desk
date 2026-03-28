4a5e99a8365e012abc3b56d9f22db1af9aa3be22
Akash Yeola <akashyeola77@gmail.com>
2026-03-28 19:44:19 +0530

    Feat: Implemented an Admin check functionality to give access to admin related features

--- src/middlewares/loginCheck.js
+++ src/middlewares/loginCheck.js
@@ -4,3 +4,10 @@ export const isLoggedIn = (req, res, next) => {
   }
   next();
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.session.user.role !== "admin") {
+    return res.send("Access denied");
+  }
+  next();
+};
